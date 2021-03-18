e9284c64c22c702a5778d640243a5c33ea3da6fc
mirmik <mirmikns@yandex.ru>
2021-03-18 12:05:50 +0300

    'up version'

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ import os
 directory = os.path.dirname(os.path.realpath(__file__))
 
 requires = [
-    "evalcache>=1.11.0",
+    "evalcache",
     "PyQt5!=5.14.1",
     "psutil"
 ]
@@ -19,7 +19,7 @@ requires = [
 setup(
     name="zenframe",
     packages=["zenframe"],
-    version="0.3.0",
+    version="0.3.1",
     license="MIT",
     description="Sandbox for computationally-graphical scripts",
     author="mirmik",
